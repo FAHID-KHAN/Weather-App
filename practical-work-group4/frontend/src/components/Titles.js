5cffb0cddd4f066b03660dbcd5e695f656ebf2f5
Fahid A. Khan <49480392+FAHID-KHAN@users.noreply.github.com>
2020-01-21 16:24:43 +0200

    Add files via upload

---
+++ practical-work-group4/frontend/src/components/Titles.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Titles = () => (
+  <div>
+    <h1 className="title-container__title">Weather App</h1>
+    <h3 className="title-container__subtitle">
+      Find out temperature, conditions , humidity and more...
+    </h3>
+    <h4>Developed By Moyen and Fahid </h4>
+  </div>
+);
+
+export default Titles;
